5f9fa5865d931de9b12c1524974b2f139921888d
Luiz Felipe Limao <lflimao@hotmail.com>
2025-02-08 16:37:48 -0300

    fix docker

--- docker/seeds.sql
+++ docker/seeds.sql
@@ -2,12 +2,11 @@ CREATE TABLE surveys (
     id VARCHAR PRIMARY KEY,
     merchant_id VARCHAR NOT NULL,
     user_id VARCHAR NOT NULL,
-    rating FLOAT NOT NULL,
+    rating NUMERIC(4,2) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE merchant_avg_ratings (
     merchant_id VARCHAR PRIMARY KEY,
-    average_rating FLOAT
-);
+    rating NUMERIC(4,2) NOT NULL,);
